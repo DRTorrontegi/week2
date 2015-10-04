4bbfce6b080a626a44d79aca6fb71e398670dd9a
DRTorrontegi <drt@email.com>
2015-10-04 09:12:22 +0200

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions create a special matrix that can keep 
+## cache of the matrices data and its inverse
 
-## Write a short comment describing this function
+## makeCacheMatrix creates that "matrix" which is really a list
+## containing a function to
+
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse
+## 4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  
+  ### 1. set the value of the matrix
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  ### 2. get the value of the matrix
+  get <- function() x
+  
+  ### 3. set the value of the inverse of the matrix
+  setsolve <- function(solve) m <<- solve
+  
+  ### 4. get the value of the inverse of the matrix
+  getsolve <- function() m
+  
+  # return our list
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the special "matrix"
+## created with the above function. However, it first checks to see if the
+## inverse has already been calculated. If so, it `get`s the inverse from the
+## cache and skips the computation. Otherwise, it calculates the inverse of
+## the data and sets the value of the inverse in the cache via the `setinverse`
+## function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve() # retrieve from cache
+  
+  if(!is.null(m)) { # we got something...
+    message("getting cached data")
+    return(m)
+  }
+  
+  # cache isn't filled yet
+  data <- x$get()       # get it
+  m <- solve(data, ...) # solve it
+  x$setsolve(m)	      # set it
+  m		      # return it
 }
